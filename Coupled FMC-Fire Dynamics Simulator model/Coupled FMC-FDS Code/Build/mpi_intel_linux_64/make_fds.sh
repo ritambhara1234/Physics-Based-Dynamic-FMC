72a86e517c598a23e752d32e39ed7cb350fe9cd4
ritambhara1234 <144624156+ritambhara1234@users.noreply.github.com>
2024-03-14 12:24:53 -0400

    Delete Coupled FMC-Fire Dynamics Simulator model directory

--- Coupled FMC-Fire Dynamics Simulator model/Coupled FMC-FDS Code/Build/mpi_intel_linux_64/make_fds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dir=`pwd`
-target=${dir##*/}
-
-../Scripts/save_fdsinfo.sh
-echo Building $target
-make -j4 VPATH="/gpfs/home/rrd20z/WFDS_3/fds/Source" -f /gpfs/home/rrd20z/WFDS_3/fds/Build/makefile $target
